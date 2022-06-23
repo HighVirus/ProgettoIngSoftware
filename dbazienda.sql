a6722f35c64f27867fd7b97b597e3b888dd15270
Yuna <103384687+CrystalYuna@users.noreply.github.com>
2022-06-23 22:23:04 +0200

    Update dbazienda.sql
    aggiunta tabella slave prod_mag che connette magazzino_aziendale con produzione

--- dbazienda.sql
+++ dbazienda.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.29, for Linux (x86_64)
 --
--- Host: localhost    Database: db_azienda
+-- Host: localhost    Database: dbazienda
 -- ------------------------------------------------------
 -- Server version	8.0.29
 
@@ -31,7 +31,7 @@ CREATE TABLE `account` (
   `password` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,14 +40,14 @@ CREATE TABLE `account` (
 
 LOCK TABLES `account` WRITE;
 /*!40000 ALTER TABLE `account` DISABLE KEYS */;
-INSERT INTO `account` (type, nome, cognome, email, password) VALUES
-(1,'Alessandro','Spank','1','1'),
-(2,'Alessandro','Spank','2','2'),
-(3,'Alessandro','Spank','3','3'),
-(2,'Edoardo','Mannino','edoardomannino@gmail.com','ciaociao'),
-(2,'Alberto','Scannaliato','albertoscannaliato@gmail.com','provapass5'),
-(3,'Gabriele','Saporito','gabrielesaporito@gmail.com','retedue'),
-(1,'Maria','Magro','mariamagro@gmail.com','treppitre');
+INSERT INTO `account` VALUES 
+(1,1,'Alessandro','Spank','1','1'),
+(2,2,'Alessandro','Spank','2','2'),
+(3,3,'Alessandro','Spank','3','3'),
+(4,2,'Edoardo','Mannino','edoardomannino@gmail.com','ciaociao'),
+(5,2,'Alberto','Scannaliato','albertoscannaliato@gmail.com','provapass5'),
+(6,3,'Gabriele','Saporito','gabrielesaporito@gmail.com','retedue'),
+(7,1,'Maria','Magro','mariamagro@gmail.com','treppitre');
 /*!40000 ALTER TABLE `account` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -67,28 +67,22 @@ CREATE TABLE `catalogo_aziendale` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-LOCK TABLES `catalogo_aziendale` WRITE;
-/*!40000 ALTER TABLE `catalogo_aziendale` DISABLE KEYS */;
-INSERT INTO `catalogo_aziendale` VALUES 
-('12745182','tachipirina 1000 mg 16 compresse','paracetamolo',4.54),
-('12745232','tachipirina 10 mg/ml soluzione per infusione','paracetamolo',12.5),
-('19655051','bentelan 1 mg 10 compresse resistenti ','betametasone',1.35),
-('24840074','cardioaspirin 100 mg 30 compresse gastroresistenti','acido acetilsalicilico',2.35),
-('27860016','zitromax 250 mg 6 capsule rigide','azitromicina',8.5),
-('34246013','nurofen 200 mg + 30 mg 12 compresse rivestite','ibuprofene',6.67),
-('42386488','brufen 400 mg 16 compresse rivestite con film','ibuprofene',4.75);
-/*!40000 ALTER TABLE `magazzino_aziendale` ENABLE KEYS */;
-UNLOCK TABLES;
-
 --
 -- Dumping data for table `catalogo_aziendale`
 --
 
 LOCK TABLES `catalogo_aziendale` WRITE;
 /*!40000 ALTER TABLE `catalogo_aziendale` DISABLE KEYS */;
+INSERT INTO `catalogo_aziendale` VALUES
+ ('12745182','tachipirina 1000 mg 16 compresse','paracetamolo',4.54),
+ ('12745232','tachipirina 10 mg/ml soluzione per infusione','paracetamolo',12.5),
+ ('19655051','bentelan 1 mg 10 compresse resistenti ','betametasone',1.35),
+ ('24840074','cardioaspirin 100 mg 30 compresse gastroresistenti','acido acetilsalicilico',2.35),
+ ('27860016','zitromax 250 mg 6 capsule rigide','azitromicina',8.5),
+ ('34246013','nurofen 200 mg + 30 mg 12 compresse rivestite','ibuprofene',6.67),
+ ('42386488','brufen 400 mg 16 compresse rivestite con film','ibuprofene',4.75);
 /*!40000 ALTER TABLE `catalogo_aziendale` ENABLE KEYS */;
 UNLOCK TABLES;
-
 
 --
 -- Table structure for table `corriere_as`
@@ -205,11 +199,6 @@ INSERT INTO `farmacia_ord` VALUES
 UNLOCK TABLES;
 
 --
--- Table structure for table `farmordini`
---
-
-
---
 -- Table structure for table `magazzino_aziendale`
 --
 
@@ -221,7 +210,7 @@ CREATE TABLE `magazzino_aziendale` (
   `lotto` varchar(9) DEFAULT NULL,
   `nome_farmaco` varchar(255) NOT NULL,
   `principio_attivo` varchar(100) NOT NULL,
-  `prescrivibilita` boolean NOT NULL,
+  `prescrivibilita` tinyint(1) NOT NULL,
   `data_scadenza` date DEFAULT NULL,
   `costo` double DEFAULT '0',
   `unita` int NOT NULL DEFAULT '0',
@@ -236,38 +225,15 @@ CREATE TABLE `magazzino_aziendale` (
 LOCK TABLES `magazzino_aziendale` WRITE;
 /*!40000 ALTER TABLE `magazzino_aziendale` DISABLE KEYS */;
 INSERT INTO `magazzino_aziendale` VALUES 
-('12745182','abe789','tachipirina 1000 mg 16 compresse','paracetamolo', false ,'2027-07-01',4.54,4503),
-('12745182','abe790','tachipirina 1000 mg 16 compresse','paracetamolo', false ,'2028-07-01',4.54,4503),
-('12745232','abe775','tachipirina 10 mg/ml soluzione per infusione','paracetamolo', true,'2025-06-01',12.5,157),
-('19655051','bfh845','bentelan 1 mg 10 compresse resistenti ','betametasone',false ,'2022-07-01',1.35,19),
-('24840074','bgt541','cardioaspirin 100 mg 30 compresse gastroresistenti','acido acetilsalicilico',false ,'2022-09-01',2.35,871),
-('27860016','frt654','zitromax 250 mg 6 capsule rigide','azitromicina',true ,'2023-05-01',8.5,210),
-('34246013','trf741','nurofen 200 mg + 30 mg 12 compresse rivestite','ibuprofene',true ,'2024-12-01',6.67,0),
-('42386488','rfq416','brufen 400 mg 16 compresse rivestite con film','ibuprofene',false ,'2022-07-01',4.75,5);
+('12745182','abe789','tachipirina 1000 mg 16 compresse','paracetamolo',0,'2027-07-01',4.54,4503),
+('12745182','abe790','tachipirina 1000 mg 16 compresse','paracetamolo',0,'2028-07-01',4.54,4503),
+('12745232','abe775','tachipirina 10 mg/ml soluzione per infusione','paracetamolo',1,'2025-06-01',12.5,157),
+('19655051','bfh845','bentelan 1 mg 10 compresse resistenti ','betametasone',0,'2022-07-01',1.35,19),
+('24840074','bgt541','cardioaspirin 100 mg 30 compresse gastroresistenti','acido acetilsalicilico',0,'2022-09-01',2.35,871),
+('27860016','frt654','zitromax 250 mg 6 capsule rigide','azitromicina',1,'2023-05-01',8.5,210),
+('34246013','trf741','nurofen 200 mg + 30 mg 12 compresse rivestite','ibuprofene',1,'2024-12-01',6.67,0),
+('42386488','rfq416','brufen 400 mg 16 compresse rivestite con film','ibuprofene',0,'2022-07-01',4.75,5);
 /*!40000 ALTER TABLE `magazzino_aziendale` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `magcat`
---
-
-DROP TABLE IF EXISTS `magcat`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `magcat` (
-  `codice_aic` varchar(8) NOT NULL,
-  `lotto` varchar(9) NOT NULL,
-  `nome_farmaco` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `magcat`
---
-
-LOCK TABLES `magcat` WRITE;
-/*!40000 ALTER TABLE `magcat` DISABLE KEYS */;
-/*!40000 ALTER TABLE `magcat` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -282,6 +248,7 @@ CREATE TABLE `ord_far` (
   `codice_aic_o` varchar(9) NOT NULL,
   `lotto_o` varchar(9) NOT NULL,
   `unita` int NOT NULL,
+  KEY `ord_far_ibfk_1` (`codice_ordine_o`),
   CONSTRAINT `ord_far_ibfk_1` FOREIGN KEY (`codice_ordine_o`) REFERENCES `ordini` (`codice_ordine`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -292,11 +259,11 @@ CREATE TABLE `ord_far` (
 
 LOCK TABLES `ord_far` WRITE;
 /*!40000 ALTER TABLE `ord_far` DISABLE KEYS */;
-INSERT INTO `ord_far` VALUES
-('47811','24840074', 'bgt541', 550),
-('47811','12745182', 'abe789', 23),
-('47811','12745182', 'abe790', 40),
-('47811','27860016', 'frt654', 350);
+INSERT INTO `ord_far` VALUES 
+('47811','24840074','bgt541',550),
+('47811','12745182','abe789',23),
+('47811','12745182','abe790',40),
+('47811','27860016','frt654',350);
 /*!40000 ALTER TABLE `ord_far` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -321,26 +288,77 @@ CREATE TABLE `ordini` (
 
 LOCK TABLES `ordini` WRITE;
 /*!40000 ALTER TABLE `ordini` DISABLE KEYS */;
-INSERT INTO `ordini` VALUES
+INSERT INTO `ordini` VALUES 
 ('47811','2022-07-05',3);
 /*!40000 ALTER TABLE `ordini` ENABLE KEYS */;
 UNLOCK TABLES;
 
-CREATE TABLE IF NOT EXISTS produzione_farmaco (
-    codice_aic_p varchar(9) NOT NULL PRIMARY KEY,
-    start_production_date DATE NOT NULL,
-    production_period INT NOT NULL,
-    unita_production INT NOT NULL
-)
+--
+-- Table structure for table `prod_mag`
+--
 
-INSERT INTO produzione_farmaco VALUES
-('12745182', '2022-06-20', 2, 100),
-('12745232', '2022-06-20', 4, 200),
-('19655051', '2022-06-20', 6, 200),
-('24840074', '2022-06-20', 1, 100),
-('27860016', '2022-06-20', 7, 500),
-('34246013', '2022-06-20', 6, 700),
-('42386488', '2022-06-20', 9, 900);
+DROP TABLE IF EXISTS `prod_mag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `prod_mag` (
+  `codice_aic_pm` varchar(9) NOT NULL,
+  `lotto_pm` varchar(9) NOT NULL,
+  PRIMARY KEY (`codice_aic_pm`,`lotto_pm`),
+  KEY `lotto_pm` (`lotto_pm`),
+  CONSTRAINT `prod_mag_ibfk_1` FOREIGN KEY (`codice_aic_pm`) REFERENCES `produzione_farmaco` (`codice_aic_p`),
+  CONSTRAINT `prod_mag_ibfk_2` FOREIGN KEY (`lotto_pm`) REFERENCES `magazzino_aziendale` (`lotto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `prod_mag`
+--
+
+LOCK TABLES `prod_mag` WRITE;
+/*!40000 ALTER TABLE `prod_mag` DISABLE KEYS */;
+INSERT INTO `prod_mag` VALUES 
+('12745232','abe775'),
+('12745182','abe789'),
+('19655051','bfh845'),
+('24840074','bgt541'),
+('27860016','frt654'),
+('42386488','rfq416'),
+('34246013','trf741');
+/*!40000 ALTER TABLE `prod_mag` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `produzione_farmaco`
+--
+
+DROP TABLE IF EXISTS `produzione_farmaco`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `produzione_farmaco` (
+  `codice_aic_p` varchar(9) NOT NULL,
+  `start_production_date` date NOT NULL,
+  `production_period` int NOT NULL,
+  `unita_production` int NOT NULL,
+  PRIMARY KEY (`codice_aic_p`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `produzione_farmaco`
+--
+
+LOCK TABLES `produzione_farmaco` WRITE;
+/*!40000 ALTER TABLE `produzione_farmaco` DISABLE KEYS */;
+INSERT INTO `produzione_farmaco` VALUES +
+('12745182','2022-06-20',2,100),
+('12745232','2022-06-20',4,200),
+('19655051','2022-06-20',6,200),
+('24840074','2022-06-20',1,100),
+('27860016','2022-06-20',7,500),
+('34246013','2022-06-20',6,700),
+('42386488','2022-06-20',9,900);
+/*!40000 ALTER TABLE `produzione_farmaco` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -351,4 +369,4 @@ INSERT INTO produzione_farmaco VALUES
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-16 15:22:21
+-- Dump completed on 2022-06-23 20:18:16
