0fc981e684a44c42609121e4564ec118e940b011
tulipani <gabriele.saporito@community.unipa.it>
2022-07-02 19:57:32 +0200

    EEEE

--- src/main/java/me/unipa/progettoingsoftware/gestionedati/entity/AlertE.java
+++ src/main/java/me/unipa/progettoingsoftware/gestionedati/entity/AlertE.java
@@ -8,6 +8,8 @@ import java.util.List;
 
 public class AlertE {
     @Getter
+    private final String codeAlert;
+    @Getter
     private final AlertType alertType;
     @Getter
     private final String message;
@@ -18,21 +20,24 @@ public class AlertE {
     @Getter
     private final List<Farmaco> farmacoQuantityList;
 
-    public AlertE (AlertType alertType, String piva, String farmaciaName){
+    public AlertE (String codeAlert, AlertType alertType, String piva, String farmaciaName){
+        this.codeAlert = codeAlert;
         this.alertType = alertType;
         this.message = "La farmacia " + piva + "|" + farmaciaName + " ha un problema con uno o più ordini, contattala.";
         this.caricoOrderList = null;
         this.farmacoQuantityList = null;
     }
 
-    public AlertE (AlertType alertType, List<Order> caricoOrderList, String uselessIdentifier){
+    public AlertE (String codeAlert,AlertType alertType, List<Order> caricoOrderList, String uselessIdentifier){
+        this.codeAlert = codeAlert;
         this.alertType = alertType;
         this.message = "Ci sono degli ordini che non hai caricato, verifica.";
         this.caricoOrderList = caricoOrderList;
         this.farmacoQuantityList = null;
     }
 
-    public AlertE (AlertType alertType, List<Farmaco> farmacoQuantityList){
+    public AlertE (String codeAlert, AlertType alertType, List<Farmaco> farmacoQuantityList){
+        this.codeAlert = codeAlert;
         this.alertType = alertType;
         this.message = "Ci sono dei farmaci in esaurimento! Verifica.";
         this.farmacoQuantityList = farmacoQuantityList;
--- src/main/java/me/unipa/progettoingsoftware/utils/AlertListBController.java
+++ src/main/java/me/unipa/progettoingsoftware/utils/AlertListBController.java
@@ -1,5 +1,6 @@
 package me.unipa.progettoingsoftware.utils;
 
+import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXTableColumn;
 import io.github.palexdev.materialfx.controls.MFXTableView;
 import io.github.palexdev.materialfx.controls.cell.MFXTableRowCell;
@@ -11,13 +12,17 @@ import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import me.unipa.progettoingsoftware.gestioneareafarmaceutica.gestionefarmaci.StorageFarmaciaC;
 import me.unipa.progettoingsoftware.gestionedati.entity.AlertE;
 import me.unipa.progettoingsoftware.gestionedati.entity.Farmaco;
+import me.unipa.progettoingsoftware.gestionedati.entity.Order;
 
 import java.util.Comparator;
 import java.util.List;
@@ -32,4 +37,49 @@ public class AlertListBController extends AnchorPane {
 
     @Getter
     private final List<AlertE> alertList;
+
+    public void setupTable() {
+        MFXTableColumn<AlertE> codiceAlertColumn = new MFXTableColumn<>("Codice Alert", true, Comparator.comparing(AlertE::getCodeAlert));
+        codiceAlertColumn.setPrefWidth(130);
+        MFXTableColumn<AlertE> tipoColumn = new MFXTableColumn<>("Tipo Alert", true, Comparator.comparing(AlertE::getAlertType));
+        tipoColumn.setPrefWidth(130);
+
+        MFXTableColumn<AlertE> infoAlertColumn = new MFXTableColumn<>("", false);
+        infoAlertColumn.setRowCellFactory(param -> new MFXTableRowCell<>(alertE -> alertE) {
+            private final MFXButton infoAlertButton = new MFXButton("");
+
+            @Override
+            public void update(AlertE alertE) {
+                if (alertE == null) {
+                    setGraphic(null);
+                    return;
+                }
+                Image buttonImage = new Image(getClass().getResourceAsStream("/images/info.png"));
+                ImageView imageView = new ImageView(buttonImage);
+                imageView.setFitWidth(15);
+                imageView.setFitHeight(17);
+                infoAlertButton.setTextFill(Paint.valueOf("WHITE"));
+                infoAlertButton.setGraphic(imageView);
+
+                setGraphic(infoAlertButton);
+                infoAlertButton.setOnAction(event -> alertC.showViewAlertFarmacia(alertE));
+
+
+            }
+        });
+
+
+        codiceAlertColumn.setRowCellFactory(order -> new MFXTableRowCell<>(AlertE::getCodeAlert));
+        tipoColumn.setRowCellFactory(order -> new MFXTableRowCell<>(AlertE::getAlertType));
+
+
+        alertTable.getTableColumns().addAll(codiceAlertColumn, tipoColumn, infoAlertColumn);
+        alertTable.getFilters().addAll(
+                new StringFilter<>("Codice Alert", AlertE::getCodeAlert),
+                new IntegerFilter<>("Tipo Alert", AlertE::getAlertType)
+
+        );
+
+        alertTable.setItems(FXCollections.observableArrayList(alertList));
+    }
 }
